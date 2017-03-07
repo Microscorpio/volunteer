2473ed4b090c62f460ca8c4edec898314149c2d6
yzw <yzw@Ava.local>
2017-03-07 17:13:23 +0800

    finish tab change

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # volunteer
 some pages about volunteer.
+
+## icon文件class对照表
 ![Alt text](/font_demo/fontDemo.jpg "iconfont")
--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,15 @@
 $(function(){
 	//解决我的发布在IE8下不兼容"last-child"导致页面显示有误
 	$(".sys-wrapper .my-release-layout .my-release-list:last").addClass("last");
+
+	//tab-layout 切换
+	$changeTab = $(".tab-layout .tab-nav ul li");
+	$changeTab.each(function(i){
+		$(this).click(function(e){
+			e.preventDefault();
+			e.stopPropagation();
+			$(this).addClass("active").siblings().removeClass("active");
+			$(".tab-layout .tab-content div.tab-content-list").eq(i).show().siblings().hide();
+		})
+	});
 });
